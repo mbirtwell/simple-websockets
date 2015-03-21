78fe9f8c9a38ea90b9be89cdbb7323e067d441b8
Michael Birtwell <michael.birtwell@gmail.com>
2015-03-21 23:24:02 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='simple-websockets',
+    version='0.1',
+    packages=['websockets'],
+    url='',
+    license='',
+    author='Michael Birtwell',
+    author_email='michael.birtwell@gmail.com',
+    description='Simple websockets for simple people',
+)
